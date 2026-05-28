7072da06c36e71572486946894d589140feb560b
luigy2021-netizen <luigy2021@gmail.com>
2026-05-27 20:26:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def horarios_disponibles(fecha, duracion, giro):
 
     return disponibles
 
-logo = Image.open("kroniq booking.png")
+logo = Image.open("kroniq.png")
 st.image(logo, use_container_width=True)
 
 st.write("Demo de agenda digital para negocios que trabajan por cita.")
